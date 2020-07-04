dd23ad33b26994678ec4e90c5919b564327d5816
Sidhant Aggarwal <sidhant.aggarwal@cred.club>
2020-07-04 15:30:06 +0530

    initial project setup

---
+++ src/main/java/com/ant/search/cerebro/CerebroApplication.java
@@ -0,0 +1,13 @@
+package com.ant.search.cerebro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerebroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CerebroApplication.class, args);
+	}
+
+}
